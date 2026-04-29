4286404c7c6adff8168482241ce285265aadda42
woohyuk0428 <whkang03@gmail.com>
2026-04-29 14:03:28 +0900

    feat: 최초 커밋

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MyClock from './MyClock.vue'
+</script>
+<template>
+  <div id="app">
+    <h1>시계</h1>
+    <MyClock></MyClock>
+  </div>
+</template>
+<style scoped>
+div#app {
+  padding: 0 30px 30px 30px;
+  margin: 30px auto;
+  max-width: 400px;
+  border: 1px solid #ccc;
+  box-shadow: 3px 3px 3px #aaa;
+}
+</style>
---
+++ src/MyClock.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import moment from "moment";
+const time = ref(null);
+onMounted(() => {
+  const callback = () => {
+    time.value = moment().format("HH:mm:ss");
+  };
+  callback();
+  setInterval(callback, 1000);
+})
+</script>
+<template>
+  <div>{{ time }}</div>
+</template>
+<style scoped>
+div {
+  display: inline-block;
+  font-size: 20pt;
+  padding: 3px 12px;
+  border: 1px solid gray;
+}
+</style>
