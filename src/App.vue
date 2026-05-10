ed6e60a21383dd29bc357e5c0c7570efd72b425e
woohyuk0428 <whkang03@gmail.com>
2026-05-10 14:38:28 +0900

    myClock 구현 완료

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,22 @@ import MyClock from './MyClock.vue'
 <template>
   <div id="app">
     <h1>시계</h1>
-    <MyClock></MyClock>
+    <MyClock id="clock1" class="myClock" />
+    <MyClock id="clock2" class="myClock" interval="3000"/>
+    <MyClock id="clock3" class="myClock" interval="6000" format="yyyy-MM-DD HH:mm"/>
   </div>
 </template>
 <style scoped>
 div#app {
   padding: 0 30px 30px 30px;
   margin: 30px auto;
-  max-width: 400px;
+  max-width: 600px;
   border: 1px solid #ccc;
   box-shadow: 3px 3px 3px #aaa;
 }
+
+.myClock { margin-right: 5px;}
+#clock1 { background-color: #ffd; }
+#clock2 { background-color: #dfd; font-size: 15pt; }
+#clock3 { background-color: #dff; font-size: 12pt; }
 </style>
--- src/MyClock.vue
+++ src/MyClock.vue
@@ -1,17 +1,34 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, onBeforeUnmount } from "vue";
 import moment from "moment";
+
+const { format = "HH:mm:ss", interval = 1000 } = defineProps(["format", "interval"]);
 const time = ref(null);
-onMounted(() => {
+const timerId = ref(0);
+
+function start() {
   const callback = () => {
-    time.value = moment().format("HH:mm:ss");
+    time.value = moment().format(format);
+    console.log(time.value);
   };
   callback();
-  setInterval(callback, 1000);
-})
+  timerId.value = setInterval(callback, interval);
+}
+
+function stop() {
+  clearInterval(timerId.value);
+  timerId.value = 0;
+}
+
+onMounted( () => {  start() });
+onBeforeUnmount( () =>{ stop() });
 </script>
 <template>
-  <div>{{ time }}</div>
+  <div>
+    <span>{{ time }}</span>
+  <input type="button" @click="start" v-if="timerId == 0" value="start">
+  <input type="button" @click="stop" v-if="timerId != 0" value="stop">
+  </div>
 </template>
 <style scoped>
 div {
@@ -20,4 +37,5 @@ div {
   padding: 3px 12px;
   border: 1px solid gray;
 }
+button { margin-left: 4px; }
 </style>
